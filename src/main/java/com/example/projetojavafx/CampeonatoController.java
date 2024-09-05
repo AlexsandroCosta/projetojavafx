10157985b2a68b7e25eef242b18612b7e705aa0f
AlexsandroCosta <alexsandro.costasv@gmail.com>
2024-09-05 15:58:40 -0300

    campeonatoController

--- src/main/java/com/example/projetojavafx/CampeonatoController.java
+++ src/main/java/com/example/projetojavafx/CampeonatoController.java
@@ -1,4 +1,13 @@
 package com.example.projetojavafx;
 
+import com.example.projetojavafx.model.entities.Campeonato;
+
 public class CampeonatoController {
+    private Campeonato campeonato;
+
+    public void setCampeonatoSelecionado(Campeonato campeonato){
+        this.campeonato = campeonato;
+    }
+
+
 }
